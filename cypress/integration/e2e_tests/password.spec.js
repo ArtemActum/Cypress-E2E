a46be26ec1d01359be03f9295c77a81ed7f14bde
Artem Minsadyrov <artem.mindsadyrov@actumdigital.com>
2022-06-08 13:57:03 +0200

    Navbar Test

---
+++ cypress/integration/e2e_tests/password.spec.js
@@ -0,0 +1,23 @@
+describe('Password Test', () => {
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('should click on the signin button', () => {
+        cy.get('#signin_button').click()
+    })
+
+    it('should click on the forgotten password', () => {
+        cy.get('.top_offset a').click()
+    })
+
+    it('should fill email form', () => {
+        cy.get('#user_email').type("test.email@email.com")
+    })
+
+    it('should submit the form', () => {
+        cy.contains('Send Password').click()
+    })
+
+})
+
