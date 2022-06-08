9d23ddd3e1e927f769d98263b38c414e4d713c1a
Artem Minsadyrov <artem.mindsadyrov@actumdigital.com>
2022-06-08 15:02:11 +0200

    new-payee

--- cypress/integration/e2e_tests/login.spec.js
+++ cypress/integration/e2e_tests/login.spec.js
@@ -6,7 +6,11 @@ describe('Login / Logout Test', () => {
     })
 
     it('should try to login with invalid data', () => {
-        cy.login('invalid username', 'invalid password')
+        // cy.get('#login_form').should('be.visible')
+        // cy.get('#user_login').type('invalid username')
+        // cy.get('#user_password').type('invalid password')
+        // cy.contains('Sign in').click()
+        cy.login("invalid username", "invalid password")
     })
 
     it('should display error message', () => {
@@ -19,6 +23,11 @@ describe('Login / Logout Test', () => {
         cy.fixture('user').then(user => {
             const username = user.id
             const password = user.pwd
+
+            // cy.get('#user_login').type(username)
+            // cy.get('#user_password').type(password)
+            // cy.get('#user_remember_me').click()
+            // cy.contains('Sign in').click()
             cy.login(username, password)
         })
         cy.get('ul.nav-tabs').should('be.visible')
